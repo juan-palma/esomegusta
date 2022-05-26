943b666aa7cef7684726bbbb60b49ffddcbf4c41
Juan Palma <juan.palma@me.com>
2022-05-26 14:55:20 -0500

    config login facebook

--- assets/js/owner/main.js
+++ assets/js/owner/main.js
@@ -473,9 +473,9 @@ window.fbAsyncInit = function() {
 function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
 	console.log('Welcome!  Fetching your information.... ');
 	FB.api('/me', function(response) {
+		console.log(response);
 		console.log('Successful login for: ' + response.name);
-		document.getElementById('status').innerHTML =
-		'Thanks for logging in, ' + response.name + '!';
+		//document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!';
 	});
 }
 // function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
